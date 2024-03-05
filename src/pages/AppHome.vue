23372a59985853eca7838b7a5eb6ffd8f41414f9
PasqualeIannace <pasquale.iannace@gmail.com>
2024-03-05 15:02:38 +0100

    style: changed cards in AppHome

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -89,19 +89,14 @@ export default {
 
                 <div class="col-md-4 gy-4" v-for="user in store.userList">
                     <div class="card h-100">
-                        <div class="card-header text-center myCard">
-                            <b>{{ user.activity_name }}</b>
-                            <img :src="user.logo_activity" alt="Logo" class="w-100">
-                        </div>
-                        <div class="card-body">
-
-                        </div>
-                        <div class="card-footer text-center">
-                          <router-link :to="{ name: 'user-details', params: { id: user.id } }" class="btn btn-primary">
-                                <font-awesome-icon icon="fa-solid fa-circle-info" class="me-2" />
-                                <span>Mostra dettagli</span>
-                          </router-link>
-                        </div>
+                        <router-link :to="{ name: 'user-details', params: { id: user.id } }">
+                            <div class="card-header text-center myCard">
+                                <b>{{ user.activity_name }}</b>
+                            </div>
+                            <div class="card-body">
+                                <img :src="user.logo_activity" alt="Logo" class="w-100">
+                            </div>
+                        </router-link>
                     </div>
                 </div>
             </div>
