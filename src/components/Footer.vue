e18be1851418db01b7c02d28e9eb3c29334cf6bc
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-03-08 16:57:21 +0100

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -63,7 +63,7 @@ export default {
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
                         <div class="row justify-content-center">
                             <div class="col-12 text-center">
-                                <span class="fs-5">DELIVEBOO</span>
+                                <span class="fs-5 Mytext">DELIVEBOO</span>
                             </div>                            
                         </div>
                         <div class="row">
@@ -77,41 +77,53 @@ export default {
         </div>
         <div class="footer-bottom dark-blue text-white">
             <div class="container-sm py-2">
-                <div class="row justify-content-center">
+                <div class="row justify-content-center position-relative">
+                    <img src="ghost.png" id="myFooterimg">
+                    <img src="ghost.png" id="myFooterimg2">
                     <div class="col-12 text-center">
-                        <span class="fs_7">Copyright <i class="fa-regular fa-copyright"></i>Your Website 2019</span>
+                        <a class="nav-link myA text-light" href="http://127.0.0.1:8000/" target="_blank">
+						Lavora con noi
+					</a>    
+					<router-link to="/about">
+						Chi siamo
+					</router-link>
                     </div>
                 </div>
             </div>
         </div>
         <div class="container">
-            <div class="row">
-                <ul>
-                    <li class="nav-item orangetext">
-					<a class="nav-link myA" href="http://127.0.0.1:8000/" target="_blank">
-						Lavora con noi
-					</a>
-				</li>
-
-                <li>
-								<router-link to="/about">
-									Chi siamo
-								</router-link>
-							</li>
-                </ul>
-            </div>
-            <p>Copyright ©2024 - Team <strong>4</strong> Classe <strong>109</strong>. All rights reserved.
-                Progetto finale di Boolean sviluppato da <strong>Pasquale Iannace</strong>, <strong>Francesca
-                    Picoco</strong>, <strong>Thomas Salvaterra</strong>, <strong>Muhammad Israr</strong>, <strong>Abdullah
-                    Azza</strong>.</p>
+            <p class="text-light">Copyright <i class="fa-regular fa-copyright"></i> 2024 - Team <strong>4</strong> Classe <strong>109</strong>. All rights reserved.Progetto finale di Boolean sviluppato da <strong>Pasquale Iannace</strong>, <strong>Francesca Picoco</strong>, <strong>Thomas Salvaterra</strong>, <strong>Muhammad Israr</strong>, <strong>Abdullah Azza</strong>.</p>
         </div>
     </footer>
 </template>
 
 <style scoped>
+.Mytext{
+    color: orange;
+}
+#myFooterimg{
+    width: 200px;
+    position: absolute;
+    bottom: 0px;
+    right: 700px;
+}
+#myFooterimg2{
+    width: 200px;
+    position: absolute;
+    bottom: 0px;
+    right: 400px;
+    transform: scaleX(-1);
+}
+footer{
+    background-color: rgba(0, 0, 0, 0.511);
+}
 .myborder {
 	border-bottom: 2px solid orange;
 	padding-top: 3vh;
     border-top:  2px solid orange;
+}
+a{
+    color: goldenrod;
+    text-decoration: none;
 }
 </style>
